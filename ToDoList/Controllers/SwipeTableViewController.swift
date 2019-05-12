e8d3b4a16d6c039f8eb2dbb8c1bbd027e82097de
Thomas J LeBlancn <anyonecaninvest123@gmail.com>
2019-05-12 11:16:08 -0400

    super class of swipeTable controller and uiTable controller complete

---
+++ ToDoList/Controllers/SwipeTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SwipeTableViewController.swift
+//  ToDoList
+//
+//  Created by Thomas LeBlanc on 5/12/19.
+//  Copyright © 2019 AnyoneCanInvest. All rights reserved.
+//
+
+import UIKit
+import SwipeCellKit
+
+class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80
+    }
+
+    //MARK: table view data source methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
+        return cell
+
+    }
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            print("delete cell")
+            self.updateModel(at: indexPath)
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        //        options.transitionStyle = .border
+        return options
+    }
+    
+    // this function will allow the subclasses to override the super class functionality
+    func updateModel(at indexPath: IndexPath) {
+        print("Item deleted from auper class")
+    }
+}
+
--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -11,7 +11,7 @@ import RealmSwift
 
 let itemArrayKeyStr = "ToDoListArray"
 
-class ToDoListViewController: UITableViewController {
+class ToDoListViewController: SwipeTableViewController {
     let realm = try! Realm()
     
     var toDoItems: Results<Item>?
@@ -23,6 +23,7 @@ class ToDoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
     }
     //Mark Tableview Datasource Methods
@@ -31,16 +32,30 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         if let item = toDoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
             cell.accessoryType = item.done ? .checkmark : .none
         } else {
-            cell.textLabel?.text = "No items added"
+            cell.textLabel?.text = "No items added yet"
         }
         return cell
     }
     
+    //MARK: - Deelete data from swipe
+    override func updateModel(at indexPath: IndexPath) {
+//        super.updateModel(at: indexPath)            // executes super class method, being overridden by this subclass function
+        if let itemForDeletion = toDoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    realm.delete(itemForDeletion)        // remove an item from the  context and list
+                }
+            } catch {
+                print("Error deleting item \(error)")
+            }
+        }
+    }
+
     //Mark TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let item = toDoItems?[indexPath.row] {
